c688f697bdd0b226eab6918da5362edb8dada4f6
Mariano Fuhr <marianofuhr@MacBook-Pro-de-Mariano.local>
2021-02-02 11:50:30 -0300

    agregado cuota deportiva, cuota seguro vida flia, descuentos automaticos mensuales

--- src/main/java/com/mutual/amps/AppConfig.java
+++ src/main/java/com/mutual/amps/AppConfig.java
@@ -15,11 +15,7 @@ public class AppConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://amps-front-test.herokuapp.com")
-<<<<<<< HEAD
                        /*  .allowedOrigins("http://localhost:4200") */
-=======
-                        /* .allowedOrigins("http://localhost:4200") */
->>>>>>> dev
 		                .allowedMethods("PUT", "DELETE","POST","GET");
             }
         };
