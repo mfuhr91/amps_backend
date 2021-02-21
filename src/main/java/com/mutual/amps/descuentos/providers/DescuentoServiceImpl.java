4a006e8ec9a720894f844313c8c250e2a59696e3
Mariano Fuhr <marianofuhr@MacBook-Pro-de-Mariano.local>
2021-02-21 20:07:07 -0300

    se soluciono problema con la edicion de cuotas de un descuento

--- src/main/java/com/mutual/amps/descuentos/providers/DescuentoServiceImpl.java
+++ src/main/java/com/mutual/amps/descuentos/providers/DescuentoServiceImpl.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-g
 
 import com.mutual.amps.convenios.models.Convenio;
 import com.mutual.amps.convenios.providers.IConvenioService;
