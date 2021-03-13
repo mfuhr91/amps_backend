98a297d70665813dec471baa9e41e87f7390c116
Mariano Fuhr <mfuhr91@gmail.com>
2021-03-13 19:57:20 -0300

    cambios para deploy en heroku

--- src/main/java/com/mutual/amps/security/WebSecurity.java
+++ src/main/java/com/mutual/amps/security/WebSecurity.java
@@ -79,7 +79,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://amps-front-test.herokuapp.com")
-                        ./* allowedOrigins("http://localhost:4200")  */
+                        /* allowedOrigins("http://localhost:4200")  */
 		                .allowedMethods("PUT", "DELETE","POST","GET");
             }
         };
